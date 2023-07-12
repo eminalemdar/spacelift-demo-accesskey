36ba54f42d8750f49e1850ce65349f975846359f
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2023-07-12 17:13:46 +0300

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
-resource "aws_iam_access_key" "new_user" {
-  user    = aws_iam_user.new_user.name
-}
+#resource "aws_iam_access_key" "new_user" {
+#  user    = aws_iam_user.new_user.name
+#}
 
 resource "aws_iam_user" "new_user" {
   name = "new_user"
