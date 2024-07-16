ec0f445f002b557ddcf48663676562f3c825c0d6
Emin <emnalmdr@gmail.com>
2024-07-16 18:07:18 +0300

    updated IAM config
    Signed-off-by: Emin <emnalmdr@gmail.com>

--- iam.tf
+++ iam.tf
@@ -5,3 +5,17 @@
 resource "aws_iam_user" "new_user" {
   name = "new_user"
 }
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
+resource "aws_iam_group_membership" "team" {
+  name = "iam-group-team"
+
+  users = [
+    aws_iam_user.new_user.name,
+  ]
+
+  group = aws_iam_group.developers.name
+}
