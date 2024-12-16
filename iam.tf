7168443e53aa0deae0e49fe9432a29b27783fec2
Emin <emnalmdr@gmail.com>
2024-12-16 13:22:21 +0300

    updated
    Signed-off-by: Emin <emnalmdr@gmail.com>

--- iam.tf
+++ iam.tf
@@ -10,10 +10,6 @@ resource "aws_iam_user" "new_user2" {
   name = "new_user2"
 }
 
-resource "aws_iam_user" "new_user3" {
-  name = "new_user3"
-}
-
 resource "aws_iam_group" "developers" {
   name = "developers"
 }
@@ -24,7 +20,6 @@ resource "aws_iam_group_membership" "team" {
   users = [
     aws_iam_user.new_user.name,
     aws_iam_user.new_user2.name,
-    aws_iam_user.new_user3.name,
   ]
 
   group = aws_iam_group.developers.name
